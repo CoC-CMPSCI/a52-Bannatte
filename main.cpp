499e5824872c25d71ce0e8cbe42f688fddce48ba
Bannatte <32626837+Bannatte@users.noreply.github.com>
2026-03-31 23:29:50 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,17 @@
 
 int main()
 {
-    // TODO
+    in maximum_value, num;
 
+    cin >> maximum_value;
+    for (int i = 0; i < 9; ++i) {
+        cin >> num;
+        if (maximum_value < num) {
+            maximum_value = num;
+        }
+    }
+
+    cout << "Maximum value is: " << maximum_value << endl;
+    
     return 0;
 }
